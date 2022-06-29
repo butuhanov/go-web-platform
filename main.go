e75048d41672f1ca391d029c2b9b8731b5a1147f
Бутуханов Василий Анатольевич <butukhanovva@kadastr.ru>
2022-06-29 16:26:19 +0700

    #2 Пример использования файла конфигурации

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	"web-platform/config"
 	"web-platform/logging"
 )
 
-func writeMessage(logger logging.Logger) {
-	fmt.Println("Hello, Platform")
-	logger.Info("Hello, Platform")
+func writeMessage(logger logging.Logger, cfg config.Configuration) {
 
+	section, ok := cfg.GetSection("main")
+	if ok {
+		message, ok := section.GetString("message")
+		if ok {
+			logger.Info(message)
+		} else {
+			logger.Panic("Cannot find configuration setting")
+		}
+	} else {
+		logger.Panic("Config section not found")
+	}
 }
 
 func main() {
-	var logger logging.Logger = logging.NewDefaultLogger(logging.Information)
-	writeMessage(logger)
 
+	var cfg config.Configuration
+	var err error
+	cfg, err = config.Load("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var logger logging.Logger = logging.NewDefaultLogger(cfg)
+	writeMessage(logger, cfg)
 }
