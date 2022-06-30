b30c97401efeeb67c8ef920ef6cbbad4afc67adb
Бутуханов Василий Анатольевич <butukhanovva@kadastr.ru>
2022-06-30 16:13:46 +0700

    #3 Финальный пример использования

--- main.go
+++ main.go
@@ -10,15 +10,15 @@ import (
 func writeMessage(logger logging.Logger, cfg config.Configuration) {
 
 	section, ok := cfg.GetSection("main")
-	if ok {
-		message, ok := section.GetString("message")
-		if ok {
-			logger.Info(message)
-		} else {
-			logger.Panic("Cannot find configuration setting")
-		}
+	if (ok) {
+			message, ok := section.GetString("message")
+			if (ok) {
+					logger.Info(message)
+			} else {
+					logger.Panic("Cannot find configuration setting")
+			}
 	} else {
-		logger.Panic("Config section not found")
+			logger.Panic("Config section not found")
 	}
 }
 
@@ -26,11 +26,14 @@ func main() {
 
 	services.RegisterDefaultServices()
 
-	// var cfg config.Configuration
-	// services.GetService(&cfg)
-
-	// var logger logging.Logger
-	// services.GetService(&logger)
-
 	services.Call(writeMessage)
+
+	val := struct {
+			message string
+			logging.Logger
+	}{
+			message: "Hello from the struct",
+	}
+	services.Populate(&val)
+	val.Logger.Debug(val.message)
 }
